bdefce48edbab71aee46b6f01bf4972269058e07
Atsuko Shimizu <atskae@gmail.com>
2022-03-06 09:39:29 -0500

    Add more operator overloads

--- ray-tracing-in-one-weekend/01-ray-tracing-in-one-weekend/include/vec3.h
+++ ray-tracing-in-one-weekend/01-ray-tracing-in-one-weekend/include/vec3.h
@@ -7,6 +7,11 @@
 
 class vec3 {
     public:
+        // Class fields
+        double e[3];
+        
+        // Class methods
+
         // Constructors
         vec3(): e{0, 0, 0} {}
         vec3(double e0, double e1, double e2): e{e0, e1, e2} {}
@@ -71,14 +76,75 @@ class vec3 {
         double length() {
             return sqrt(length_squared());
         }
-
-    public:
-        double e[3];
 };
 
 // Type aliases for vec3
 using point3 = vec3; // 3D point
 using color = vec3; // RGB color
+
+/*
+    vec3 Utility functions
+*/
+
+// Pretty-print
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << "vec3(" << v.e[0] << ", " << v.e[1] << ", " << v.e[2] << ")";
+}
+
+// Operator overloads
+// Note that these return a new vec3 object
+
+// Sum two vectors (vl=left, vr=right)
+inline vec3 operator+(const vec3& vl, const vec3& vr) {
+    return vec3(
+        vl.e[0] + vr.e[0],
+        vl.e[1] + vr.e[1],
+        vl.e[2] + vr.e[2]
+    );
+}
+
+// Subtract two vectors
+inline vec3 operator-(const vec3& vl, const vec3& vr) {
+    return vec3(
+        vl.e[0] - vr.e[0],
+        vl.e[1] - vr.e[1],
+        vl.e[2] - vr.e[2]
+    );
+}
+
+// Multiply two vectors
+inline vec3 operator*(const vec3& vl, const vec3& vr) {
+    return vec3(
+        vl.e[0] * vr.e[0],
+        vl.e[1] * vr.e[1],
+        vl.e[2] * vr.e[2]
+    );
+}
+
+// Multply a vector by a scalar as a left argument
+inline vec3 operator*(const double scalar, const vec3& vr) {
+    return vec3(
+        scalar * vr.e[0],
+        scalar * vr.e[1],
+        scalar * vr.e[2]
+    );
+}
+
+// Multply a vector by a scalar as a right argument
+inline vec3 operator*(const vec3& vl, const double scalar) {
+    return scalar * vl;
+}
+
+// Divide a vector by a scalar
+inline vec3 operator/(vec3 v, double scalar) {
+    return (1/scalar) * v;
+}
+
+// Compute the dot product
+
+// Compute the cross product
+
+// Compute the normalized vector
 
 // Header guard
 #endif
