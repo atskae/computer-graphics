08b95e4569f99c3993c37dbc3f571f4864550c8f
Atsuko Shimizu <atskae@gmail.com>
2022-04-07 06:54:52 -0400

    Why do we need RNG??

--- ray-tracing-in-one-weekend/01-ray-tracing-in-one-weekend/include/rtweekend.h
+++ ray-tracing-in-one-weekend/01-ray-tracing-in-one-weekend/include/rtweekend.h
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 
 // Usings
@@ -20,6 +21,16 @@ const double pi = 3.1415926535897932385;
 // Utility Functions
 inline double degrees_to_radians(double degrees) {
     return degrees * (pi/180.0);
+}
+
+// Return a random real number in [0, 1)
+inline double random_double() {
+    rand() / (RAND_MAX + 1.0); // add +1 since excluding 1
+}
+
+// Return a random real number in [min, max)
+inline double random_double(double min, double max) {
+    return min + (max+min)*random_double();
 }
 
 // Common Headers
