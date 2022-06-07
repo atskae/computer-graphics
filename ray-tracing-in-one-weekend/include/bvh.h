8b6b0d2abfa889daff09f4d916638c8c0b772ca3
Atsuko Shimizu <atskae@gmail.com>
2022-06-07 06:13:12 -0400

    Start bvh::hit()

--- ray-tracing-in-one-weekend/include/bvh.h
+++ ray-tracing-in-one-weekend/include/bvh.h
@@ -34,7 +34,33 @@ class bvh_node : public hittable {
 };
 
 bool bvh::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
-    // TODO: implement
+    // Check if the ray even hits the tree's bounding box
+    if (!this->box.hit(r, t_min, t_max)) {
+        return false;
+    }
+
+    // Since the ray hits the bounding box, do a search through the node's children
+    // to find the precise object that was hit
+    hit_record temp_hit_record;
+    // Check if the ray hits anything within the bounds defined by the left-half
+    // Recall that the BVH does not split the space physically in two ordered halves (left and right halves)
+    //  ex. the left-child of a BVH node does not literally mean the left-half of the space
+    //  The objects referenced by the child node are inside the bounds defined by the parent node
+    //  Bounding boxes can overlap
+    bool hit_left = this->left.hit(r, t_min, t_max, temp_hit_record);
+    
+    // If there was an object on the left-half's bounds, use the t value that hit that object
+    double t = hit_left ? temp_hit_record.t : t_max;
+
+    bool hit_right = this->right.hit(r, t_min, t, temp_hit_record);
+
+    // Update the return hit_record if an object was hit
+    bool object_hit = hit_left || hit_right;
+    if (object_hit) {
+        rec = temp_hit_record;
+    }
+
+    return object_hit;
 }
 
 bool bvh::bounding_box(double time0, double time1, aabb& output_box) const {
