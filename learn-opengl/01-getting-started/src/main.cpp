3318fc7c9f2e659bee20af2b3e9c9f33015edcfc
Atsuko Shimizu <atskae@gmail.com>
2022-10-23 07:42:45 -0400

    Finish setting up glad and glfw

---
+++ learn-opengl/01-getting-started/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(int argc, char* argv[]) {
+    // Wow, I haven't written a C++ main() function in a while...
+    // I have forgotten how to do it by-memory
+    std::cout << "I hope glfw and glad compiles!" << std::endl;
+    return 0;
+}
